9ee3c3087d2b65707732bd88c17dc7486f6c4cd5
intern2 <dmcclellan@ycp.edu>
2018-05-31 09:50:27 -0400

    added uiv multi-select fucntionality

--- src/components/TodoCreate.vue
+++ src/components/TodoCreate.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
     <div class="input-group header-input">
-      <select class="form-control" v-model="userId">
-        <option selected>Select a User ID</option>
-        <option v-for="i in 10">{{i}}</option>
-      </select>
+      <multi-select class="multi" v-model="selected" :options="options"/>
       <span class="input-group-btn">
           <btn type="primary" v-on:click="findTodo">
             <i class="fa fa-1x fa-search"> Search</i>
@@ -45,6 +42,7 @@
   import {Popover} from 'uiv';
   import {Tooltip} from 'uiv';
   import TodoCreedit from './TodoCreedit'
+  import {MultiSelect} from 'uiv'
 
   export default {
     name: "TodoCreate",
@@ -53,14 +51,26 @@
       Modal,
       TodoCreedit,
       Tooltip,
-      Popover
+      Popover,
+      MultiSelect
     },
     data(){
       return{
         store: TodoStore,
         userId: "",
         selected: [],
-        options: []
+        options: [
+          {value:1, label:'User1'},
+          {value:2, label:'User2'},
+          {value:3, label:'User3'},
+          {value:4, label:'User4'},
+          {value:5, label:'User5'},
+          {value:6, label:'User6'},
+          {value:7, label:'User7'},
+          {value:8, label:'User8'},
+          {value:9, label:'User9'},
+          {value:10, label:'User10'},
+        ]
       }
     },
     methods: {
@@ -68,9 +78,19 @@
         this.store.createTodo(this.title, this.userId, this.done);
       },
       findTodo: function() {
-        this.store.findTodo(this.userId)
+        console.log(this.selected[0].value)
+        this.store.findTodo(this.selected[0].value)
+      },
+      // getUserIds: function() {
+      //     var tempTodo = []
+      //     this.state.listTodos()
+      //     for(var i=0;i<tempTodo.length; i++){
+      //       tempTodo.push(this.state.todos[i].userId)
+      //     }
+      //     console.log(tempTodo)
+      //     return tempTodo
+      //   }
       }
-    }
   }
 
 </script>
@@ -79,5 +99,9 @@
   .header-input {
     position: fixed;
     padding-right: 175px;
+  }
+  .multi {
+    min-width: 1085px;
+    max-width: 1085px;
   }
 </style>
